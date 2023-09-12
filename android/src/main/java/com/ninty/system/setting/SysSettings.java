bdf3a6c17995f784b01cb0ea09ddfa2168333148
Gabriel Le Breton <lebreton.gabriel@gmail.com>
2023-09-12 16:13:13 -0400

    Refactor SysSettings.java
    1. Made the fields `final` to ensure their immutability.
    2. Added a private constructor and included JavaDoc comments to explain the purpose of the constructor.
    3. Implemented a static block to populate a map with request codes as keys and their respective enum instances as values. This allows the `get` method to fetch the enum instance in constant time, improving efficiency, especially as the number of enum values grows.
    4. Updated the `get` method to use the map for fetching the enum instance, with `UNKNOWN` as the default value if the request code is not found in the map.

--- android/src/main/java/com/ninty/system/setting/SysSettings.java
+++ android/src/main/java/com/ninty/system/setting/SysSettings.java
@@ -1,30 +1,50 @@
 package com.ninty.system.setting;
 
 import android.provider.Settings;
+import java.util.Map;
+import java.util.HashMap;
 
+/**
+ * Enum representing various system settings along with their associated actions and request codes.
+ */
 public enum SysSettings {
 
     UNKNOWN("", 0),
     WIFI(Settings.ACTION_WIFI_SETTINGS, 1),
     LOCATION(Settings.ACTION_LOCATION_SOURCE_SETTINGS, 2),
     BLUETOOTH(Settings.ACTION_BLUETOOTH_SETTINGS, 3),
-    WRITESETTINGS(Settings.ACTION_MANAGE_WRITE_SETTINGS, 4),
+    WRITE_SETTINGS(Settings.ACTION_MANAGE_WRITE_SETTINGS, 4),
     AIRPLANE(Settings.ACTION_AIRPLANE_MODE_SETTINGS, 5);
 
-    public String action;
-    public int requestCode;
+    private static final Map<Integer, SysSettings> SETTINGS_MAP = new HashMap<>();
 
-    SysSettings(String action, int requestCode) {
+    static {
+        for (SysSettings setting : SysSettings.values()) {
+            SETTINGS_MAP.put(setting.requestCode, setting);
+        }
+    }
+
+    public final String action;
+    public final int requestCode;
+
+    /**
+     * Constructor to initialize SysSettings enum instance.
+     *
+     * @param action the associated action string
+     * @param requestCode the associated request code
+     */
+    private SysSettings(String action, int requestCode) {
         this.action = action;
         this.requestCode = requestCode;
     }
 
+    /**
+     * Retrieves the SysSettings enum based on the provided request code.
+     *
+     * @param requestCode the request code to look for
+     * @return the corresponding SysSettings enum, or UNKNOWN if not found
+     */
     public static SysSettings get(int requestCode){
-        for (SysSettings setting : SysSettings.values()) {
-            if(setting.requestCode == requestCode){
-                return setting;
-            }
-        }
-        return UNKNOWN;
+        return SETTINGS_MAP.getOrDefault(requestCode, UNKNOWN);
     }
 }
