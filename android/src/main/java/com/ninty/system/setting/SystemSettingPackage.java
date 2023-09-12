e4487a26b5da5c9ce9a0529cb4db4c7532fbc2de
Gabriel Le Breton <lebreton.gabriel@gmail.com>
2023-09-12 16:17:26 -0400

    Link SystemSetting native module with React Native app
    1. Removed the `createJSModules` method which was necessary for versions before 0.47 of React Native. If backward compatibility with older versions is not a concern, this method can be safely removed.
    2. Added JavaDoc comments to the class and methods to explain their purposes and the parameters they take.
    3. Changed the `createNativeModules` method to use `Collections.singletonList` which is more efficient for lists that contain a single element compared to creating an `ArrayList` and adding an element to it.

--- android/src/main/java/com/ninty/system/setting/SystemSettingPackage.java
+++ android/src/main/java/com/ninty/system/setting/SystemSettingPackage.java
@@ -1,30 +1,35 @@
 package com.ninty.system.setting;
 
 import com.facebook.react.ReactPackage;
-import com.facebook.react.bridge.JavaScriptModule;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+/**
+ * This class is responsible for linking native modules with the React Native application.
+ */
 public class SystemSettingPackage implements ReactPackage {
+
+    /**
+     * Creates a list of native modules to be included in the React Native application.
+     *
+     * @param reactContext the context of the React application
+     * @return a list containing the native modules to be linked
+     */
     @Override
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
-        List<NativeModule> modules = new ArrayList<>();
-
-        modules.add(new SystemSetting(reactContext));
-
-        return modules;
+        return Collections.singletonList(new SystemSetting(reactContext));
     }
 
-    // Removed in React-Native >= 0.47
-    public List<Class<? extends JavaScriptModule>> createJSModules() {
-        return Collections.emptyList();
-    }
-
+    /**
+     * Creates a list of view managers to be included in the React Native application.
+     *
+     * @param reactContext the context of the React application
+     * @return an empty list as no custom views are being managed
+     */
     @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
         return Collections.emptyList();
