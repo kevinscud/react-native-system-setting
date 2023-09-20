ec754bfae446fc38ea2a05227ee58be12f959748
Gabriel Le Breton <lebreton.gabriel@gmail.com>
2023-09-19 23:09:03 -0400

    Update UI and add listeners for system settings

--- example/App.tsx
+++ example/App.tsx
@@ -1,15 +1,33 @@
 import React, { useEffect, useState } from 'react';
-import { Alert, Button, StyleSheet, Switch, Text, View } from 'react-native';
+import { Alert, Button, StyleSheet, Switch, Text, View, ScrollView } from 'react-native';
 import systemSetting, { CompleteFunc, VolumeData } from 'react-native-system-setting';
 import Slider from '@react-native-community/slider';
 
 const styles = StyleSheet.create({
     container: {
-        padding: 20,
+        padding: 10,
+    },
+    scrollViewContainer: {
+        flexGrow: 1,
     },
     slider: {
-        width: 200,
-        height: 40,
+        width: '100%',
+        height: 30,
+        alignSelf: 'center',
+    },
+    spacer: {
+        height: 10,
+    },
+    text: {
+        fontSize: 16,
+        marginVertical: 10,
+    },
+    labelText: {
+        fontSize: 18,
+        fontWeight: 'bold',
+    },
+    valueText: {
+        fontSize: 16,
     },
 });
 
@@ -17,6 +35,26 @@ const sliderTrackTintColor = {
     minimumTrackTintColor: '#FFFFFF',
     maximumTrackTintColor: '#000000',
 };
+
+const formatValue = (value: boolean | number | null) => {
+    if (typeof value === 'number') {
+        return `${Math.round(value * 100)}%`;
+    }
+    return value ? 'On' : 'Off';
+};
+
+const SettingSwitch = ({ value, onValueChange, label }: { value: boolean | null, onValueChange: (newValue: boolean) => void, label: string }) => (
+    <View>
+        <Text style={styles.labelText}>{label}</Text>
+        <Text style={styles.valueText}>{formatValue(value)}</Text>
+        {value !== null && (
+            <Switch value={value} onValueChange={onValueChange} />
+        )}
+        <View style={styles.spacer} />
+    </View>
+);
+
+
 
 const App: React.FC = () => {
     const [volume, setVolume] = useState<number | null>(null);
@@ -38,13 +76,40 @@ const App: React.FC = () => {
         fetchValues();
 
         const volumeListener = systemSetting.addVolumeListener((data: VolumeData) => {
+            console.log(`Volume changed: ${data.value}`);
             setVolume(data.value);
+        });
+
+        const bluetoothListener = systemSetting.addBluetoothListener((bluetoothEnabled: boolean) => {
+            console.log(`Bluetooth state changed: ${bluetoothEnabled}`);
+        });
+
+        const wifiListener = systemSetting.addWifiListener((wifiEnabled: boolean) => {
+            console.log(`WiFi state changed: ${wifiEnabled}`);
+        });
+
+        const locationListener = systemSetting.addLocationListener((locationEnabled: boolean) => {
+            console.log(`Location state changed: ${locationEnabled}`);
+        });
+
+        const locationModeListener = systemSetting.addLocationModeListener((locationMode: number) => {
+            console.log(`Location mode changed: ${locationMode}`);
+        });
+
+        const airplaneListener = systemSetting.addAirplaneListener((airplaneModeEnabled: boolean) => {
+            console.log(`Airplane mode state changed: ${airplaneModeEnabled}`);
         });
 
         return () => {
             systemSetting.removeVolumeListener(volumeListener);
+            systemSetting.removeListener(bluetoothListener);
+            systemSetting.removeListener(wifiListener);
+            systemSetting.removeListener(locationListener);
+            systemSetting.removeListener(locationModeListener);
+            systemSetting.removeListener(airplaneListener);
         };
     }, []);
+
 
     const checkPermissions = async () => {
         try {
@@ -99,54 +164,53 @@ const App: React.FC = () => {
         });
     };
 
+    const { writeSettingsPermissions } = permissions;
+
     return (
-        <View style={styles.container}>
-            <Button title="Check Permissions" onPress={checkPermissions} />
+        <ScrollView contentContainerStyle={styles.scrollViewContainer}>
+            <View style={styles.container}>
+                <Button title="Refresh Values" onPress={fetchValues} />
+                <View style={styles.spacer} />
 
-            <Text>Volume: {volume}</Text>
-            {volume !== null && (
-                <Slider
-                    style={styles.slider}
-                    minimumValue={0}
-                    maximumValue={1}
-                    value={volume}
-                    onValueChange={handleVolumeChange}
-                    {...sliderTrackTintColor}
-                    disabled={!permissions.writeSettingsPermissions}
-                />
-            )}
+                <Button title="Check Permissions" onPress={checkPermissions} />
+                <View style={styles.spacer} />
 
-            <Text>Brightness: {brightness}</Text>
-            {brightness !== null && (
-                <Slider
-                    style={styles.slider}
-                    minimumValue={0}
-                    maximumValue={1}
-                    value={brightness}
-                    onValueChange={handleBrightnessChange}
-                    {...sliderTrackTintColor}
-                    disabled={!permissions.writeSettingsPermissions}
-                />
-            )}
+                <Text style={styles.labelText}>Volume</Text>
+                <Text style={styles.valueText}>{formatValue(volume)} (raw: {volume})</Text>
+                {volume !== null && (
+                    <Slider
+                        style={styles.slider}
+                        minimumValue={0}
+                        maximumValue={1}
+                        value={volume}
+                        onValueChange={handleVolumeChange}
+                        {...sliderTrackTintColor}
+                        disabled={!writeSettingsPermissions}
+                    />
+                )}
+                <View style={styles.spacer} />
 
-            <Text>WiFi State: {wifiState ? 'On' : 'Off'}</Text>
-            {wifiState !== null && (
-                <Switch value={wifiState} onValueChange={handleSwitchChange(setWifiState, systemSetting.switchWifi)} />
-            )}
+                <Text style={styles.labelText}>Brightness</Text>
+                <Text style={styles.valueText}>{formatValue(brightness)} (raw: {brightness})</Text>
+                {brightness !== null && (
+                    <Slider
+                        style={styles.slider}
+                        minimumValue={0}
+                        maximumValue={1}
+                        value={brightness}
+                        onValueChange={handleBrightnessChange}
+                        {...sliderTrackTintColor}
+                        disabled={!writeSettingsPermissions}
+                    />
+                )}
+                <View style={styles.spacer} />
 
-            <Text>Location State: {locationState ? 'On' : 'Off'}</Text>
-            {locationState !== null && (
-                <Switch value={locationState} onValueChange={handleSwitchChange(setLocationState, systemSetting.switchLocation)} />
-            )}
-
-            <Text>Bluetooth State: {bluetoothState ? 'On' : 'Off'}</Text>
-            {bluetoothState !== null && (
-                <Switch value={bluetoothState} onValueChange={handleSwitchChange(setBluetoothState, systemSetting.switchBluetooth)} />
-            )}
-
-            <Button title="Refresh Values" onPress={fetchValues} />
-        </View>
+                <SettingSwitch value={wifiState} onValueChange={handleSwitchChange(setWifiState, systemSetting.switchWifi)} label="WiFi State" />
+                <SettingSwitch value={locationState} onValueChange={handleSwitchChange(setLocationState, systemSetting.switchLocation)} label="Location State" />
+                <SettingSwitch value={bluetoothState} onValueChange={handleSwitchChange(setBluetoothState, systemSetting.switchBluetooth)} label="Bluetooth State" />
+            </View>
+        </ScrollView>
     );
-};
+}
 
 export default App;
